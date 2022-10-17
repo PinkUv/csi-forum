817fb65d4af90ed26107eccb3ee5db4be9a9f7a9
PinkUv <gabrielz5@nycstudents.net>
2022-10-17 19:28:44 -0400

    Nate's Starbuck order for Nick

---
+++ js/app.js
@@ -0,0 +1,19 @@
+tsParticles.load('tsparticles', {
+  particles: {
+    links: {
+      enable: true,
+      opacity: 1,
+      distance: 200
+    },
+    move: {
+      enable: true,
+      speed: {min: 1, max: 3},
+    },
+    opacity: {
+      value: {min: 0.3, max: 0.7},
+    },
+    size: {
+      value: {min: 1, max: 3},
+    },
+  },
+});
